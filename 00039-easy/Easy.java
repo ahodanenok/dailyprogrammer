7b0546b0a024bbb2f92767f6bbc55c670315d4e8
Andrei Hodanenok <xoda91@gmail.com>
2019-08-31 23:44:57 +0300

    39-easy

---
+++ 00039-easy/Easy.java
@@ -0,0 +1,21 @@
+/**
+ * Dailyprogrammer: 39 - easy
+ * https://www.reddit.com/r/dailyprogrammer/comments/s6bas/4122012_challenge_39_easy/
+ */
+public class Easy {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
